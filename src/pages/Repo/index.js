247681f05887e8082fc66dba13b7d81876b17869
Ami <ami1996_tm@live.co.uk>
2022-11-11 10:45:19 +0000

    Styles

--- src/pages/Repo/index.js
+++ src/pages/Repo/index.js
@@ -2,15 +2,17 @@ import React from 'react'
 import { useSelector } from 'react-redux';
 import { BackBtn, Logo, RepoInfo, Breadcrumb } from '../../components'
 
+import './style.css'
+
 export default function Repo({repo}) {
     const username = useSelector(state => state.username);
 
     return (
-        <>
+        <div className='repo'>
             <Logo isHome={false} />
             <BackBtn />
             <Breadcrumb Name={username} URL={`https://github.com/${username}/`} />
             <RepoInfo repo={repo} />
-        </>
+        </div>
     )
 }
